e094f37861bda37f3bc90ca8dd64460cd9aa156e
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2015-10-30 10:25:53 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -821,13 +821,13 @@ console.log("The real id", active.item.baseURI);
 //console.log("The indexof ,)
 //if(patt.test(active.item.id)){
 var $elementid = active.item.get(0).id;
-console.log("new active item is", $pat);
+console.log("new active item is", $elementid);
 
 console.log("The active.item" , $(active.item[0].id));
 
         if(active.item) {
             var patt = new RegExp("topMenuBetaInterface");
-            if(page.language() === 'FR' && /topMenuBetaInterface/i.test($pat)){
+            if(page.language() === 'FR' && /topMenuBetaInterface/i.test($elementid)){
                 console.log("The real Menu is here");
                 $("#topMenuBetaInterface").removeClass('active');
                 $("#topMenuBetaInterface").removeClass('hover');
