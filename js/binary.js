b4957de596ec99030455f69d796cf81692f59be2
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2015-11-16 11:25:20 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -62831,7 +62831,7 @@ var ProfitTableUI = (function(){
 
         var $statementRow = Table.createFlexTableRow([date, ref, action, desc, amount, balance], columns, "data");
         $statementRow.children(".credit").addClass(creditDebitType);
-        $statementRow.children(".date").addClass("break-line");
+       // $statementRow.children(".date").addClass("break-line");
 
         //create view button and append
         if (action === "Sell" || action === "Buy") {
