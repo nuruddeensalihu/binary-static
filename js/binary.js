fd66661651d81a3079edb219c349b660f6aaaac7
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2015-11-23 11:02:18 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -58471,6 +58471,51 @@ onLoad.queue_for_url(function () {
 
     };
 
+    var self_exclusion_date_picker = function () {
+        // 6 months from now
+        var start_date = new Date();
+        start_date.setMonth(start_date.getMonth() + 6);
+
+        // 5 years from now
+        var end_date = new Date();
+        end_date.setFullYear(end_date.getFullYear() + 5);
+
+        var id = $('#EXCLUDEUNTIL');
+
+        id.datepicker({
+            dateFormat: 'yy-mm-dd',
+            minDate: start_date,
+            maxDate: end_date,
+            onSelect: function(dateText, inst) {
+                id.attr("value", dateText);
+            },
+        });
+    };
+
+    var self_exclusion_validate_date = function () {
+        $('#selfExclusion').on('click', '#self_exclusion_submit', function () {
+            return client_form.self_exclusion.validate_exclusion_date();
+        });
+    };
+    
+    var sendRequest = function(){
+
+    };
+
+    var apiResponse = function(){
+
+    } ;
+
+    return {
+        init: init,
+        self_exclusion_validate_date : self_exclusion_validate_date,
+        self_exclusion_date_picker : self_exclusion_date_picker,
+        sendRequest: sendRequest,
+        apiResponse: apiResponse
+    };
+
+
+
 })();
 
 
