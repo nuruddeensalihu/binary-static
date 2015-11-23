dbdf042f3703c7296179f4a9e0e6c38d4f3e07e2
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2015-11-23 13:36:35 +0800

    Auto-generated commit

--- dev/src/javascript/binary/pages/selfexclusionws.js
+++ dev/src/javascript/binary/pages/selfexclusionws.js
@@ -8,32 +8,6 @@ var SelfExlusionWS = (function(){
 
     };
 
-    var self_exclusion_date_picker = function () {
-        // 6 months from now
-        var start_date = new Date();
-        start_date.setMonth(start_date.getMonth() + 6);
-
-        // 5 years from now
-        var end_date = new Date();
-        end_date.setFullYear(end_date.getFullYear() + 5);
-
-        var id = $('#EXCLUDEUNTIL');
-
-        id.datepicker({
-            dateFormat: 'yy-mm-dd',
-            minDate: start_date,
-            maxDate: end_date,
-            onSelect: function(dateText, inst) {
-                id.attr("value", dateText);
-            },
-        });
-    };
-
-    var self_exclusion_validate_date = function () {
-        $('#selfExclusion').on('click', '#self_exclusion_submit', function () {
-            return client_form.self_exclusion.validate_exclusion_date();
-        });
-    };
     
     var sendRequest = function(){
 
@@ -45,8 +19,6 @@ var SelfExlusionWS = (function(){
 
     return {
         init: init,
-        self_exclusion_validate_date : self_exclusion_validate_date,
-        self_exclusion_date_picker : self_exclusion_date_picker,
         sendRequest: sendRequest,
         apiResponse: apiResponse
     };
