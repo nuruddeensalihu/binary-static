f176c45bd5c9270c6e94f7e07181eabc9885b416
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2015-12-03 15:16:21 +0800

    Auto-generated commit

--- dev/src/javascript/binary/pages/selfexclusionws.js
+++ dev/src/javascript/binary/pages/selfexclusionws.js
@@ -157,15 +157,15 @@ var SelfExlusionWS = (function(){
 
         var hasChages  = false;
         var newData = {
-            "max_balance"  : $("#MAXCASHBAL").val().trim(),
-            "max_turnover" : $("#DAILYTURNOVERLIMIT").val().trim(),
-            "max_losses" : $("#DAILYLOSSLIMIT").val().trim(),
-            "max_7day_turnover" : $("#7DAYTURNOVERLIMIT").val().trim(),
-            "max_7day_losses" : $("#7DAYLOSSLIMIT").val().trim(),
-            "max_30day_turnover" : $("#30DAYTURNOVERLIMIT").val().trim(),
-            "max_30day_losses" : $("#30DAYLOSSLIMIT").val().trim(),
-            "max_open_bets": $("#MAXOPENPOS").val().trim(),
-            "session_duration_limit" :  $("#SESSIONDURATION").val().trim(),
+            "max_balance"  : $("#MAXCASHBAL").val().replace(/ /g, ""),
+            "max_turnover" : $("#DAILYTURNOVERLIMIT").val().replace(/ /g, ""),
+            "max_losses" : $("#DAILYLOSSLIMIT").val().replace(/ /g, ""),
+            "max_7day_turnover" : $("#7DAYTURNOVERLIMIT").val().replace(/ /g, ""),
+            "max_7day_losses" : $("#7DAYLOSSLIMIT").val().replace(/ /g, ""),
+            "max_30day_turnover" : $("#30DAYTURNOVERLIMIT").val().treplace(/ /g, ""),
+            "max_30day_losses" : $("#30DAYLOSSLIMIT").val().replace(/ /g, ""),
+            "max_open_bets": $("#MAXOPENPOS").val().replace(/ /g, ""),
+            "session_duration_limit" :  $("#SESSIONDURATION").val().replace(/ /g, ""),
             "exclude_until" : $("#EXCLUDEUNTIL").val()
         };
         $.map(newData , function(value, property){
