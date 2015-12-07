46f12d081d6a5a73477e94bc266236a7fed084b3
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2015-12-07 22:14:51 +0800

    Auto-generated commit

--- dev/src/javascript/binary/base.js
+++ dev/src/javascript/binary/base.js
@@ -6,9 +6,9 @@ $(function(){
         var contents = new Contents(page.client, page.user);
         contents.on_load();
         console.log("The WS started", Header.clock_started);
-        if (contents.clock_started === false){
-            contents.start_clock_ws();
-        }
+        //if (contents.clock_started === false){
+        contents.start_clock_ws();
+        //}
         console.log("the datasource", BinarySocket.isReady());
     });
 });
