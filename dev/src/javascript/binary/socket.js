b7ae456b83e6c51cf2dbf5b1dc3ac611a0a9a43a
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2015-12-07 09:33:10 +0800

    Auto-generated commit

--- dev/src/javascript/binary/socket.js
+++ dev/src/javascript/binary/socket.js
@@ -181,6 +181,7 @@ var BinarySocket = (function () {
     return {
         init: init,
         send: send,
+        isReady : isReady,
         close: close,
         socket: function () { return binarySocket; },
         clear: clear,
--- js/binary.js
+++ js/binary.js
@@ -49481,11 +49481,16 @@ Header.prototype = {
     on_load: function() {
         this.show_or_hide_login_form();
         this.register_dynamic_links();
-        if (!this.clock_started) this.start_clock();
+        if (!this.clock_started) {
+            this.start_clock_ws();
+        }
         this.simulate_input_placeholder_for_ie();
     },
     on_unload: function() {
         this.menu.reset();
+        if (!this.clock_started){
+            this.start_clock_ws();
+        }
     },
     show_or_hide_login_form: function() {
         if (this.user.is_logged_in && this.client.is_logged_in) {
@@ -49551,12 +49556,60 @@ Header.prototype = {
 
         this.menu.register_dynamic_links();
     },
+    start_clock_ws : function(){
+        var that = this;
+        var clock_handle;
+        var query_start_time;
+        var clock = $('#gmt-clock');
+
+        function init(){
+            BinarySocket.send({ "time": 1,"passthrough":{"client_time" :  moment.utc().unix()}});
+        }
+
+        BinarySocket.init({
+            onmessage : function(msg){
+                var response = JSON.parse(msg.data);
+
+                if (response && response.msg_type === 'time') {
+
+                    var start_timestamp = response.time;
+                    var pass = response.echo_req.passthrough.client_time;
+
+                    that.time_now = ((start_timestamp * 1000) + (moment.utc().unix() - pass));
+                     
+                    var increase_time_by = function(interval) {
+                        that.time_now += interval;
+                    };
+                    var update_time = function() {
+                         clock.html(moment(that.time_now).utc().format("YYYY-MM-DD HH:mm") + " GMT");
+                    };
+                    update_time();
+
+                    clearInterval(clock_handle);
+
+                    clock_handle = setInterval(function() {
+                        increase_time_by(1000);
+                        update_time();
+                    }, 1000);
+                }
+            }
+        });
+
+        that.run = function(){
+            setInterval(init, 900000);
+        };
+        if(BinarySocket.isReady() === true){
+            init();
+            that.run();
+            this.clock_started = true;
+        }
+        return;
+    },
     start_clock: function() {
         var clock = $('#gmt-clock');
         if (clock.length === 0) {
             return;
         }
-
         var that = this;
         var clock_handle;
         var sync = function() {
@@ -63401,6 +63454,7 @@ var BinarySocket = (function () {
     return {
         init: init,
         send: send,
+        isReady : isReady,
         close: close,
         socket: function () { return binarySocket; },
         clear: clear,
