2776674f8dd6fce5f35def89174debec577e8979
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2015-12-07 18:47:34 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -49549,53 +49549,7 @@ Header.prototype = {
 
         this.menu.register_dynamic_links();
     },
-    start_clock_ws : function(){
-        var that = this;
-        var clock_handle;
-        var query_start_time;
-        var clock = $('#gmt-clock');
 
-        function init(){
-            clock_started = true;
-            BinarySocket.send({ "time": 1,"passthrough":{"client_time" :  moment().valueOf()}});
-        }
-        that.run = function(){
-            setInterval(init, 900000);
-        };
-        if(BinarySocket.isReady() === true){
-            BinarySocket.init({
-                onmessage : function(msg){
-                    var response = JSON.parse(msg.data);
-
-                    if (response && response.msg_type === 'time') {
-
-                        var start_timestamp = response.time;
-                        var pass = response.echo_req.passthrough.client_time;
-
-                        that.time_now = ((start_timestamp * 1000) + (moment().valueOf() - pass));
-                         
-                        var increase_time_by = function(interval) {
-                            that.time_now += interval;
-                        };
-                        var update_time = function() {
-                             clock.html(moment(that.time_now).utc().format("YYYY-MM-DD HH:mm") + " GMT");
-                        };
-                        update_time();
-
-                        clearInterval(clock_handle);
-
-                        clock_handle = setInterval(function() {
-                            increase_time_by(1000);
-                            update_time();
-                        }, 1000);
-                    }
-                }
-            });
-            init();
-            that.run();
-        }
-        return{ start_clock_ws : start_clock_ws};
-    },
     start_clock: function() {
         var clock = $('#gmt-clock');
         if (clock.length === 0) {
@@ -49761,7 +49715,7 @@ Contents.prototype = {
         this.init_draggable();
         console.log("The WS is now", BinarySocket.isReady());
         if (!clock_started) {
-            Header.start_clock_ws();
+            this.start_clock_ws();
         }
     },
     on_unload: function() {
@@ -49769,6 +49723,54 @@ Contents.prototype = {
         if ($('.unbind_later').length > 0) {
             $('.unbind_later').off();
         }
+    },
+
+     start_clock_ws : function(){
+        var that = this;
+        var clock_handle;
+        var query_start_time;
+        var clock = $('#gmt-clock');
+
+        function init(){
+            clock_started = true;
+            BinarySocket.send({ "time": 1,"passthrough":{"client_time" :  moment().valueOf()}});
+        }
+        that.run = function(){
+            setInterval(init, 900000);
+        };
+        if(BinarySocket.isReady() === true){
+            BinarySocket.init({
+                onmessage : function(msg){
+                    var response = JSON.parse(msg.data);
+
+                    if (response && response.msg_type === 'time') {
+
+                        var start_timestamp = response.time;
+                        var pass = response.echo_req.passthrough.client_time;
+
+                        that.time_now = ((start_timestamp * 1000) + (moment().valueOf() - pass));
+                         
+                        var increase_time_by = function(interval) {
+                            that.time_now += interval;
+                        };
+                        var update_time = function() {
+                             clock.html(moment(that.time_now).utc().format("YYYY-MM-DD HH:mm") + " GMT");
+                        };
+                        update_time();
+
+                        clearInterval(clock_handle);
+
+                        clock_handle = setInterval(function() {
+                            increase_time_by(1000);
+                            update_time();
+                        }, 1000);
+                    }
+                }
+            });
+            init();
+            that.run();
+        }
+        return;
     },
     activate_by_client_type: function() {
         $('.by_client_type').addClass('invisible');
