da47b19e4b563618853ec32bc407163eb9d087fb
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2015-12-16 15:27:01 +0800

    Auto-generated commit

--- dev/src/javascript/binary/static/cashier/settings_securityws.js
+++ dev/src/javascript/binary/static/cashier/settings_securityws.js
@@ -107,7 +107,7 @@ var securityws = (function(){
                 console.log("The result is ok");
                 $("#changeCashierLock").hide();
                 $("#SecuritySuccessMsg").text(text.localize('Your settings have been updated successfully.'));
-                
+                console.log("The result 234 is ok");
                 if("lock_password" in response.echo_req)
                 {
                     //its a lock password
