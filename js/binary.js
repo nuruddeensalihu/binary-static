fcd5335fe9c5efe97d06aa21bbfafd09d24f06da
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2015-12-24 11:44:14 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -64594,11 +64594,14 @@ var BinarySocket = (function () {
                 console.log("The MF account", loginid);
                 str  = text.localize("from gaming account " + client_accounts[1].loginid + "to financial account (+" + client_accounts[0].loginid + ")");
                 optionML  = $form.find("#transfer_account_transfer option[value='gtf']");
+                console.log("The optionML is ", optionML);
                 optionML.text(str);
                 optionMF = $form.find("#transfer_account_transfer option[value='ftg']");
+                console.log("the optionMF is", optionMF);
                 str = text.localize("from financial account " + client_accounts[0].loginid + "to gaming account (+" + client_accounts[1].loginid + ")");
                 optionMF.text(str);
                 optionMF.attr('selected', 'selected');
+
             }
             else if(loginid.substring(0,2) == "ML"){
                 //MLT account
