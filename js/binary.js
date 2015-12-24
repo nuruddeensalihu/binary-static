2f8bc3ec736514cf97ee4d1022bde804b99198fe
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2015-12-24 15:16:25 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -64656,12 +64656,12 @@ var BinarySocket = (function () {
                     $("#client_message").hide();
 
                     if(value.account == account_from){
-                        $("#loginid_1").html(value.loginid);
-                        $("#balance_1").html(value.balance)
+                        $("#loginid_1 span").html(value.loginid);
+                        $("#balance_1 span").html(value.balance)
                     }
                     else if(value.account == account_to){
-                        $("#loginid_2").html(value.loginid);
-                        $("#balance_2").html(value.balance)
+                        $("#loginid_2 span").html(value.loginid);
+                        $("#balance_2 span").html(value.balance)
 
                     }
                 });
