328a288196252e33bb7f8f8b95d7ff7023757e66
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2015-12-28 17:35:03 +0800

    Auto-generated commit

--- dev/src/javascript/binary/static/cashier/account_transferws.js
+++ dev/src/javascript/binary/static/cashier/account_transferws.js
@@ -227,7 +227,7 @@ var account_transferws = (function(){
                 }
                 else if(account_to == undefined || account_from === undefined || $.isEmptyObject(account_to))
                 {
-                    if(account_to == loginid || account_from == loginid){
+                    if(account_from == loginid){
                         $("#client_message").show();
                         $("#client_message p").html(text.localize("The account transfer is unavailable for your account: " + loginid));
                         $("#success_form").hide();
