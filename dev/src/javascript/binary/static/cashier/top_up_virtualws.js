8be98a1f0926f6cf9286b750930d0f992cdb50ef
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2016-01-06 13:48:08 +0800

    Auto-generated commit

--- dev/src/javascript/binary/static/cashier/top_up_virtualws.js
+++ dev/src/javascript/binary/static/cashier/top_up_virtualws.js
@@ -8,12 +8,6 @@ var top_up_virtualws = (function(){
     	$("#VRT_title").hide();
     	$("#VRT_topup_errorMessage").hide();
     	BinarySocket.send({"authorize": $.cookie('login'), "req_id": 1 });
-
-        $('#btnPortfolioosubmit').on('click',function(e){
-            window.location  =page.url.url_for('user/openpositionsws')
-
-        });
-
     };
     var isAuthorized = function(response){
     	if(response.echo_req.req_id){
