7fdba8e89357f2115cfc35080101c3427cd25e03
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2016-01-13 11:08:50 +0800

    Auto-generated commit

--- dev/src/javascript/binary/pages/selfexclusionws.js
+++ dev/src/javascript/binary/pages/selfexclusionws.js
@@ -85,7 +85,7 @@ var SelfExlusionWS = (function(){
         }
     };
 
-    validate_exclusion_date = function() {
+    var validate_exclusion_date = function() {
         var exclusion_date = $('#EXCLUDEUNTIL').val();
         var date_regex = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
         var error_element_errorEXCLUDEUNTIL = clearInputErrorField('errorEXCLUDEUNTIL');
