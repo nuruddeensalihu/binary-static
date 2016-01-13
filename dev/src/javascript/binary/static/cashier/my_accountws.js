cc799723be919a57b91be9be47093bc532291717
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2016-01-13 15:16:47 +0800

    Auto-generated commit

--- dev/src/javascript/binary/static/cashier/my_accountws.js
+++ dev/src/javascript/binary/static/cashier/my_accountws.js
@@ -1,26 +1,18 @@
 var my_accountws = (function(){
 
     "use strict";
-    var currType;
 
     var init = function(){
     	$("#VRT_topup_link").hide();
 
-
-        currType = TUser.get().currency;
+        var currType = TUser.get().currency;
         var bal =  TUser.get().balance;
-
-        console.log("the balance is", bal);
-
-        console.log("the user is ", TUser.get());
 
         if(bal < 1000 && (/^VRT/.test(TUser.get().loginid) === true ) ){
             var str = "Deposit "+ currType + " 10000 virtual money into your account ";
             $("#VRT_topup_link").show();
             $("#VRT_topup_link a").text(text.localize(str));
         }
-
-
     };
 
     return {
